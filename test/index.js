3a995c7b9b3abe4518fe00edb5535dd637096902
Daniel Kershaw <danjamker@gmail.com>
2013-11-08 14:05:20 +0000

    Bus And Train
    All bus and trains times clompletes, and initial sets writen.

--- test/index.js
+++ test/index.js
@@ -54,4 +54,123 @@ describe('TransportAPI', function () {
 	 		});
 		});
 	});
+
+	describe('#NearbyBusStops', function () {
+	 	it('should return data for a Location', function(done){
+	 		transportAPI.NearbyBusStops({"lat":51.527789,"lon":-0.102323,"page":3,"rpp":10}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+	describe('#BusStopsInAboundingBox', function () {
+	 	it('should return data for a BoundingBox', function(done){
+	 		transportAPI.BusStopsInAboundingBox({"minlon":-0.0938,"minlat":51.5207,"maxlon":-0.074,"maxlat":51.5286,"page":3,"rpp":8}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+	describe('#NearbyTrainStations', function () {
+	 	it('should return data for a Location', function(done){
+	 		transportAPI.NearbyTrainStations({"lon":-0.0938,"lat":51.5207,"page":3,"rpp":8}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+	describe('#StationsInAboundingBox', function () {
+	 	it('should return data for a BoundingBox', function(done){
+	 		transportAPI.StationsInAboundingBox({"minlon":-0.0938,"minlat":51.5207,"maxlon":-0.074,"maxlat":51.5286,"page":3,"rpp":8}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+	describe('#StationsServedbyanOperator', function () {
+	 	it('should return data for a BoundingBox', function(done){
+	 		transportAPI.StationsServedbyanOperator("VI", {}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+
+	describe('#ScheduledStationDepartures', function () {
+	 	it('should return data for a specific time', function(done){
+	 		transportAPI.ScheduledStationDepartures("ZFD","2012-05-01","08:09",{}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+	describe('#LiveStationDepartures', function () {
+	 	it('should return data for a specific Stations', function(done){
+	 		transportAPI.LiveStationDepartures("SYD",{}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+	describe('#LiveStationArrivals', function () {
+	 	it('should return data for a specific Station', function(done){
+	 		transportAPI.LiveStationArrivals("SYD",{}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
+	describe('#ScheduledService', function () {
+	 	it('should return data for a specific service', function(done){
+	 		transportAPI.ScheduledService("22710002","LUT","2012-05-01","08:17",{}, function(err, res, data){
+	 			if(err) throw err;
+	 			log('res: '+ util.inspect(res));
+	 			log('data: '+ util.inspect(data));
+	 			res.should.not.equal.null;
+	 			data.should.not.equal.null;
+	 			done();
+	 		});
+		});
+	});
+
 });
